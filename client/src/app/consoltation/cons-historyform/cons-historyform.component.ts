13c08ff8a141f0881b614de00cc8b3f2fe2bbe46
lusajo143 <techdealers143@gmail.com>
2022-06-18 10:29:49 +0300

    History

--- client/src/app/consoltation/cons-historyform/cons-historyform.component.ts
+++ client/src/app/consoltation/cons-historyform/cons-historyform.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { calAge } from 'src/app/configs/config';
+import { FabricService } from 'src/app/services/fabric.service';
 
 @Component({
   selector: 'app-cons-historyform',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsHistoryformComponent implements OnInit {
 
-  constructor() { }
+  patient_id: string = ""
+
+  isLoading: boolean = true
+
+  patient: any
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private service: FabricService
+  ) { }
 
   ngOnInit(): void {
+    this.activeRoute.params.subscribe((params: any) => {
+      this.patient_id = params.id
+      this.service.getPatient({patient_id: this.patient_id}).subscribe((result: any) => {
+        this.patient = result.data
+        this.patient.dob = calAge(result.data.dob)
+        console.log(this.patient);
+        this.isLoading = false
+      })
+    })
   }
 
 }
